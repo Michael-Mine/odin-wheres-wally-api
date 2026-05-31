bf2a107fda6f274f4839c96447b8986ce4298bad
Michael-Mine <147097601+Michael-Mine@users.noreply.github.com>
2026-05-31 22:48:44 +0100

    feat: setup barebones express server

---
+++ app.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).send(err.message);
+});
+
+const PORT = 3000;
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log(`Express app listening on port ${PORT}!`);
+});
